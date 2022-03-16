3c74bd1f744ca90da316ed0a5d53e2cfe1c6d754
Kaitlin Rosen <kayejrosen@gmail.com>
2022-03-15 19:53:14 -0700

    Inputs Added

---
+++ messingaround.py
@@ -0,0 +1,23 @@
+from mimetypes import init
+
+def calculator():
+    num1 = input("What's the first number? > ")
+
+    try:
+        num1 = int(num1)
+    except:
+        print("Okay wise guy, I'll give it a shot")
+    
+    fun = input("What are you doing to it? + - * / ")
+    if fun != "+" and fun != "-" and fun != "*" and fun != "/":
+        fun_type = None
+        print("Yeah sure that'll work.")
+    
+    num2 = input("What's the second number? > ")
+    try:
+        num2 = int(num2)
+    except:
+        print("Funny. Sure.")
+
+    if type(num1) == str or type(num2) == str or fun_type = None:
+        return "{} {} {}".format(num1,fun,num2)
