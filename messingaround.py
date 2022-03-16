8af2d17ba33871762f8ebb0faa7e72337d92374b
Kaitlin Rosen <kayejrosen@gmail.com>
2022-03-15 23:13:47 -0700

    let the alphabet loop

--- messingaround.py
+++ messingaround.py
@@ -3,18 +3,38 @@ from mimetypes import init
 def vigenere_cipher():
     ##get a key
     key = input("What is the key? > ")
+    full_key = key
     ##get a user generated plaintext to encode
+    plain_text = input("What is your message? > ")
 
     ##create a new key that repeats itself until the length matches plaintext
     ##create an alphabet variable
+    while len(full_key) < len(plain_text):
+        full_key += key
+    print(full_key)
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    print(full_key)
+    alpha_index = []
+    key_index = []
+    i = 0
+    cipher_text = ""
     ##note the alphabet index for each letter in the plaintext
-    ##note the new key value for each plaintext index
     ##note the alphabet index for each value in the key
-
+    for char in plain_text:
+        alpha_index.append(alphabet.index(char))
+        key_index.append(alphabet.index(full_key[i]))
+        try:
+            cipher_text += alphabet[key_index[i] + alpha_index[i]]
+        except:
+            cipher_text += alphabet[(key_index[i] + alpha_index[i]) - 26]
+        i += 1
+    
     ##replace each letter of the plaintext
     ##with the value at the index of its index added ...
     ##added to the index of the new key value at the same index of og letter
-    pass
+    return cipher_text
+
+print(vigenere_cipher())
 
 def shift_cipher():
     ##ask the user for something to encode
