3a89d808921bdced8b2a273e0a635698b1c1e4c2
Kaitlin Rosen <kayejrosen@gmail.com>
2022-03-19 13:43:54 -0700

    Cleaned Frequency Analysis

--- frequency_analysis.py
+++ frequency_analysis.py
@@ -1,62 +1,44 @@
-def frequency_analysis(text):
-    ##these are the actual percentages for each letter in the alphabet
-    ##as used in the english language
-    alphabet_perc = {
-        "a":8.5, "b":2.1, "c":4.5, "d":3.4, "e":11.2, "f":1.8, "g":2.5, "h":3.0, 
-        "i":7.5, "j":0.2, "k":1.1, "l":5.5, "m":3.0, "n":6.7, "o":7.2, "p":3.2, 
-        "q":0.2, "r":7.6, "s":5.7, "t":7.0, "u":3.6, "v":1.0, "w":1.3, "x":0.3, 
-        "y":1.8, "z":0.3
-        }
+'''
+most used letters in the english language:
 
-    ## need to find a way to get the correct percentages added here
-    cipher_alphabet = {
-        "a":0, "b":0, "c":0, "d":0, "e":0, "f":0, "g":0, "h":0, "i":0, "j":0, "k":0, 
-        "l":0, "m":0, "n":0, "o":0, "p":0, "q":0, "r":0, "s":0, "t":0, "u":0, "v":0, 
-        "w":0, "x":0, "y":0, "z":0
-        }
-    
-    text = text.replace(" ","") ##remove spaces and other mixemups
-    text = text.replace(".","")
-    text = text.replace(",","")
-    text = text.replace("'","")
-    text = text.lower()
+"e":11.2 - "a":8.5 - "r":7.6 - "i":7.5 - "o":7.2 - "t":7.0 - "n":6.7 - 
+"s":5.7 - "l":5.5 - "c":4.5 - "u":3.6 - "d":3.4 - "p":3.2 - "h":3.0 - "m":3.0
+"g":2.5 - "b":2.1 - "y":1.8 - "f":1.8 - "w":1.3 - "k":1.1 - "v":1.0 - 
+"x":0.3 - "z":0.3 - "q":0.2 - "j":0.2
+'''
 
-    ##create list of unique letters used in the plaintext
+def frequency_analysis(cipher_text):
+    most_used = (
+        "e","a","r","i","o","t","n","s","l","c","u","d","p","h","m","g","b",
+        "y","f","w","k","v","x","z","q","j")
+
+    ##a list (so it can be ordered) of unique letters in the ciphertext
     letters_used = []
-    for letter in text:
-        if letter not in letters_used:
-            letters_used.append(letter)
-    # print(letters_used)
-    ##count each letter from the alphabet list as it appears in the text
-    letters_count = [text.count(letter) for letter in letters_used]
+    letters_used = [
+        letter for letter in cipher_text 
+        if letter not in letters_used]
+
+    ##count the instances of each letter used in the cipher text
+    letters_count = [cipher_text.count(letter) for letter in letters_used]
     '''while letters_used and letters_count are separate lists, 
     the indexes are the same. letters_count[0] refers to the count
     of the letter at letters_used[0]'''
 
-    ##divide that count by the length of text, then multiply by 100 to get percentage
-    ##letters_count now is now populated with the percentage of each letter
-    for i in range(len(letters_count)):
-        letters_count[i] = (letters_count[i]/len(text)) * 100
+    ##find the most used letter in the cipher, and save the index
+    # highest_index = letters_count.index(max(letters_count))
+    # most_cipher = letters_used[highest_index]##now we now which letter is most used
 
-    ##put those percentages into the corresponding cipher_alphabet dictionary.
-    for letter in letters_used:
-        i = letters_used.index(letter)
-        cipher_alphabet[letter] += letters_count[i]
+    '''for solving a subsitute cipher:
+    
+    replace the most used with e
+    then the second with a
+    and so on through the alphabet'''
+    ## 
+    '''for solving a shift cipher:
+    
+    apply a shift cipher using 
+    the index of the most common used cipherletter as the shift'''
 
-    ##find the most common used leter from the ciphertext
-    highest_perc = 0
-    most_used = ""
-    for letter in cipher_alphabet:
-        if cipher_alphabet[letter] > highest_perc:
-            most_used = letter
-    ##replace the most common letter from the regular alphabet into the ciphertext
-    solve = text.replace(letter,"e")
-    ##print and let the user choose if it's correct
-    ##find the different in index between the most common cipher alphabet letter and the true alphabet
-    ##use that shift to fill in each letter with the shifted index
+    return
 
-    return solve
-
-
-
-print(frequency_analysis("""an input text needs to be long enough to have value when it comes to frequency analysis because otherwise there arent enough letters to analyze"""))
+frequency_analysis("hnvvkpuwbaaleamvyrllwpuntlzzhnlzopkklupzrllwpunpazovyaibahnvvkpuwbaaleamvykljyfwapunjpwolyzpzsvunlyzvpmfvbjhuarllwpazovyahaslhzakvuapujsbklwbujabhapvuaopzdhfdlohclslzzopuazhivbaaolzaybjabylvmaolwshpualeaibaaoljpwolyaleazapssohzluvbnoslaalyzavhuhsfgl")
