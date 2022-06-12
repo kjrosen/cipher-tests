38d7e1348cb72eedfd2e21a9eb5d8be69e6dbd6b
Kaitlin Rosen <kayejrosen@gmail.com>
2022-06-12 00:00:51 -0700

    event listeners for cipher selection added

--- static/js/encoding.js
+++ static/js/encoding.js
@@ -1,2 +1,29 @@
 'use strict';
 
+const plaintext = document.getElementById('plaintext');
+
+const cipherButtons = document.querySelectorAll('.cipher');
+
+const subOpts = document.getElementById('subOpts'); 
+const shiftOpts = document.getElementById('shiftOpts'); 
+const vigOpts = document.getElementById('vigOpts'); 
+const enigOpts = document.getElementById('enigOpts'); 
+
+for (const button of cipherButtons) {
+    button.addEventListener('click', (evt) => {
+        evt.preventDefault();
+
+        const cipher = evt.target.classList;
+
+        if (cipher.contains('sub')) {
+            subOpts.classList.remove('hide');
+        } else if (cipher.contains('shift')) {
+            shiftOpts.classList.remove('hide');
+        } else if (cipher.contains('vig')) {
+            vigOpts.classList.remove('hide');
+        } else if (cipher.contains('enig')) {
+            enigOpts.classList.remove('hide');
+        }
+
+    });
+}
